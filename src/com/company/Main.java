68be3e3539a5f5760e1ad31dd6ab232dcc492e6a
Boro Radojcic <bororadojcic1991@gmail.com>
2022-06-12 16:44:07 +0200

    Bubble Sort algorithm - sort algorithm

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,14 @@
 package com.company;
 
+import com.company.sortalgorithms.BubbleSort;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        BubbleSort bubbleSort = new BubbleSort(new int[]{9, 3, 6, 2, 7, 1});
+        bubbleSort.sortAsc();
+        bubbleSort.printArray();
+        bubbleSort.sortDesc();
+        bubbleSort.printArray();
     }
 }
--- src/com/company/sortalgorithms/BubbleSort.java
+++ src/com/company/sortalgorithms/BubbleSort.java
@@ -1,4 +1,42 @@
 package com.company.sortalgorithms;
 
 public class BubbleSort {
+    private int[] array;
+
+    public BubbleSort(int[] array) {
+        this.array = array;
+    }
+
+    public void sortAsc(){
+        for (int i = this.array.length -1; i > 0; i--){
+            for(int a = 0; a < i; a++){
+                if (this.array[a] >this.array[a+1])
+                    swap(this.array, a, a+1);
+            }
+        }
+    }
+
+    public void sortDesc(){
+        for (int i = this.array.length -1; i > 0; i--){
+            for(int a = 0; a < i; a++){
+                if (this.array[a] < this.array[a+1])
+                    swap(this.array, a, a+1);
+            }
+        }
+    }
+
+    public void printArray(){
+        for (int j : this.array) {
+            System.out.println(j);
+        }
+    }
+
+    private void swap(int[] array, int i, int j){
+        if (i == j)
+            return;
+
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
 }
