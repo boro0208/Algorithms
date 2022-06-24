97d59c6f79a205142a8d4606032e73b1a11faf96
boro0208 <bororadojcic1991@gmail.com>
2022-06-24 12:54:49 +0200

    Merge sort algorithm desc

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,42 +8,50 @@ public class Main {
 
     public static void main(String[] args) {
         int[] array = {9, 3, 6, 2, 7, 1,9};
+        int[] arrayRadix = {9345, 3516, 6256, 2124, 7235, 1114,9532};
 
 //        System.out.println(Arrays.toString(array));
 //        System.out.println("Bubble Sort Algorithm");
 //        BubbleSort bubbleSort = new BubbleSort(array);
 //        bubbleSort.sortAsc();
 //        bubbleSort.printArray();
-//
+
 //        System.out.println("Selection Sort Algorithm");
 //        SelectionSort selectionSort = new SelectionSort(array);
 //        selectionSort.sortAsc();
 //        selectionSort.printArray();
-//
+
 //        System.out.println("Insertion Sort Algorithm");
 //        InsertionSort insertionSort = new InsertionSort(array);
 //        insertionSort.sortAsc();
 //        insertionSort.printArray();
-//
+
 //        System.out.println("Shell Sort Algorithm");
 //        ShellSort shellSort = new ShellSort(array);
 //        shellSort.sortAsc();
 //        shellSort.printArray();
-//
-//        System.out.println("Merge Sort Algorithm");
-//        MergeSort mergeSort = new MergeSort();
-//        mergeSort.sortAsc(array, 0 , array.length);
-//        mergeSort.printArray();
+
+        System.out.println("Merge Sort Algorithm");
+        MergeSort mergeSort = new MergeSort();
+        mergeSort.sortAsc(array, 0 , array.length);
+        mergeSort.printArray();
+        mergeSort.sortDesc(array, 0 , array.length);
+        mergeSort.printArray();
 
 //        System.out.println("Quick Sort Algorithm");
 //        QuickSort quickSort = new QuickSort();
 //        quickSort.sortAsc(array, 0 , array.length);
 //        quickSort.printArray();
 
-        System.out.println("Counting Sort Algorithm");
-        CountingSort countingSort = new CountingSort();
-        countingSort.sortAsc(array, 1 , 9);
-        countingSort.printArray();
+//        System.out.println("Counting Sort Algorithm");
+//        CountingSort countingSort = new CountingSort();
+//        countingSort.sortAsc(array, 1 , 9);
+//        countingSort.printArray();
 
+
+//        System.out.println("Radix Sort Algorithm");
+//        RadixSort radixSort = new RadixSort();
+//        radixSort.sortAsc(arrayRadix, 10 , 4);
+//        radixSort.printArray();
     }
 }
--- src/com/company/sortalgorithms/MergeSort.java
+++ src/com/company/sortalgorithms/MergeSort.java
@@ -39,6 +39,40 @@ public class MergeSort {
 
     }
 
+    public void sortDesc(int[] array, int start, int end){
+        if (end-start < 2){
+            return;
+        }
+
+        int mid = (start + end) / 2;
+
+        sortDesc(array, start, mid);
+        sortDesc(array, mid, end);
+
+        mergeDesc(array, start, mid, end);
+        this.array = array;
+    }
+
+    private void mergeDesc(int[] array, int start, int mid, int end){
+        if (array[mid-1] >= array[mid]){
+            return;
+        }
+
+        int i = start;
+        int j = mid;
+        int tempIndex = 0;
+
+        int[] temp = new int[end-start];
+
+        while (i < mid && j < end){
+            temp[tempIndex++] = array[i] >= array[j] ? array[i++] : array[j++];
+        }
+
+        System.arraycopy(array, i, array, start + tempIndex, mid - i);
+        System.arraycopy(temp, 0, array, start, tempIndex);
+
+    }
+
     public void printArray(){
         for (int j : this.array) {
             System.out.print(j);
